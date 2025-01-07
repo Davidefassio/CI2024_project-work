22093a490b4f4022a41aadbeac8d78c60ab6402e
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2025-01-07 12:08:11 +0100

    upload formulas

--- s323584.py
+++ s323584.py
@@ -1,1 +1,37 @@
+# Davide Fassio (s323584) 
+# Computation Intellingence 2024-2025
+# Project Submission
 
+import numpy as np
+
+
+def f1(x):
+    return np.sin(x[0])
+
+
+def f2(x):
+    return np.add(np.multiply(np.multiply(np.sin(np.divide(np.add(x[0],np.add(x[1],np.add(x[0],x[2]))),np.abs(3.9996729354721))),np.pow(4.516628540244641,np.cos(1.34483262814729))), 5441856.124013525), 56465.10655809015)
+
+
+def f3(x):
+    return np.add(np.multiply(np.subtract(np.multiply(np.abs(np.multiply(np.cos(np.cos(np.multiply(x[1],np.cos(np.log(np.abs(1.2974301813372846)))))),np.multiply(-0.24155820788172014,x[1]))),np.sin(np.multiply(x[1],np.cos(np.log(np.abs(0.5966226090825351)))))),np.multiply(np.add(np.multiply(np.abs(1.408263596578893),np.cos(np.divide(x[0],1.7343066284052329))),x[1]),np.cos(1.2926209252645433))),50.61161179521197),21.20757039569563)
+
+
+def f4(x):
+    return np.add(3.278772269179432,np.multiply(np.cos(x[1]),6.999896850889465))
+
+
+def f5(x):
+    return np.add(np.multiply(np.divide(x[0],np.divide(np.abs(np.pow(np.add(x[0],4.236427175846976),np.multiply(2.0751120040023934,1.5437012652037385))),np.add(np.multiply(9.160534827326751,np.add(5.371178336148423,np.divide(3.2075304248295455,x[0]))),np.subtract(x[0],np.pow(x[0],x[1]))))),2.2884503096224874e-09),-5.794871983107769e-10)
+
+
+def f6(x):
+    return np.subtract(np.multiply(-0.30561742835889544,np.subtract(x[1],x[0])),np.subtract(x[0],np.add(x[1],x[1])))
+
+
+def f7(x):
+    return np.add(np.multiply(np.multiply(np.log(np.abs(np.divide(np.abs(np.multiply(x[0],np.multiply(np.sin(1.0154617625775857),x[1]))),np.subtract(x[0],x[1])))),np.abs(np.multiply(np.abs(np.multiply(np.sin(np.abs(np.multiply(np.sin(np.sin(0.31337637023044285)),np.log(np.abs(np.divide(np.sin(np.multiply(np.multiply(0.3172748331815308,x[0]),x[1])),np.subtract(x[0],x[1]))))))),x[0])),np.multiply(np.sin(np.cos(np.divide(-1.6386430009801558,np.add(np.abs(np.multiply(np.sin(np.add(x[0],x[1])),x[1])),3.3036239193128707)))),x[1])))),26.66750114052942),10.230460379797575)
+
+
+def f8(x):
+    return np.add(np.multiply(np.multiply(np.abs(np.log(np.abs(np.cos(np.divide(np.subtract(np.subtract(x[5],-0.04935361301815182),np.sin(np.subtract(np.subtract(x[5],-0.04978842826919352),np.divide(x[5],2.564096012253908)))),np.abs(4.772087877744795)))))),np.subtract(x[5],np.abs(np.divide(np.divide(np.sin(np.divide(np.subtract(x[4],-0.04978842826919352),6.077107450793685)),np.divide(x[5],np.subtract(x[4],np.sin(np.subtract(x[4],6.112698651108903))))),np.divide(x[5],np.subtract(x[4],np.sin(np.subtract(x[4],np.sin(-1.803674288445735))))))))),4765.072335625078),-549.7154440795717)      
